9839b671152a8e5591268a7aa1ff89efd31f6e53
Anna <ann_dmitirevna97@mail.ru>
2024-09-07 11:11:58 +0300

    folder

---
+++ view.php
@@ -0,0 +1,90 @@
+<?php
+$clientCountry = $_GET['country'] ?? '';
+$clientName = $_GET['name'] ?? '';
+$clientPhone = $_GET['phone'] ?? '';
+?>
+
+<!DOCTYPE html>
+<!-- saved from url=(0060)https://previewpreland.name/goodbye-carbs-ioa2-lp/?ufl=22896 -->
+<html lang="es"><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+<!--<base href="https://previewpreland.name/goodbye-carbs-ioa2-lp/">--><base href=".">
+<title>¡CIENTÍFICOS CONMOCIONADOS! Este estudiante de Argentina ha creado una fórmula vegetal única que ayuda a
+adelgazar entre 10 y 15 kg al mes ¡SIN dietas ni deporte!</title>
+
+<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0, user-scalable=no">
+<script type="text/javascript" data-pagespeed-no-defer="">(function(){function d(b){var a=window;if(a.addEventListener)a.addEventListener("load",b,!1);else if(a.attachEvent)a.attachEvent("onload",b);else{var c=a.onload;a.onload=function(){b.call(this);c&&c.call(this)}}}var p=Date.now||function(){return+new Date};window.pagespeed=window.pagespeed||{};var q=window.pagespeed;function r(){this.a=!0}r.prototype.c=function(b){b=parseInt(b.substring(0,b.indexOf(" ")),10);return!isNaN(b)&&b<=p()};r.prototype.hasExpired=r.prototype.c;r.prototype.b=function(b){return b.substring(b.indexOf(" ",b.indexOf(" ")+1)+1)};r.prototype.getData=r.prototype.b;r.prototype.f=function(b){var a=document.getElementsByTagName("script"),a=a[a.length-1];a.parentNode.replaceChild(b,a)};r.prototype.replaceLastScript=r.prototype.f;
+r.prototype.g=function(b){var a=window.localStorage.getItem("pagespeed_lsc_url:"+b),c=document.createElement(a?"style":"link");a&&!this.c(a)?(c.type="text/css",c.appendChild(document.createTextNode(this.b(a)))):(c.rel="stylesheet",c.href=b,this.a=!0);this.f(c)};r.prototype.inlineCss=r.prototype.g;
+r.prototype.h=function(b,a){var c=window.localStorage.getItem("pagespeed_lsc_url:"+b+" pagespeed_lsc_hash:"+a),f=document.createElement("img");c&&!this.c(c)?f.src=this.b(c):(f.src=b,this.a=!0);for(var c=2,k=arguments.length;c<k;++c){var g=arguments[c].indexOf("=");f.setAttribute(arguments[c].substring(0,g),arguments[c].substring(g+1))}this.f(f)};r.prototype.inlineImg=r.prototype.h;
+function t(b,a,c,f){a=document.getElementsByTagName(a);for(var k=0,g=a.length;k<g;++k){var e=a[k],m=e.getAttribute("data-pagespeed-lsc-hash"),h=e.getAttribute("data-pagespeed-lsc-url");if(m&&h){h="pagespeed_lsc_url:"+h;c&&(h+=" pagespeed_lsc_hash:"+m);var l=e.getAttribute("data-pagespeed-lsc-expiry"),l=l?(new Date(l)).getTime():"",e=f(e);if(!e){var n=window.localStorage.getItem(h);n&&(e=b.b(n))}e&&(window.localStorage.setItem(h,l+" "+m+" "+e),b.a=!0)}}}
+function u(b){t(b,"img",!0,function(a){return a.src});t(b,"style",!1,function(a){return a.firstChild?a.firstChild.nodeValue:null})}
+q.i=function(){if(window.localStorage){var b=new r;q.localStorageCache=b;d(function(){u(b)});d(function(){if(b.a){for(var a=[],c=[],f=0,k=p(),g=0,e=window.localStorage.length;g<e;++g){var m=window.localStorage.key(g);if(!m.indexOf("pagespeed_lsc_url:")){var h=window.localStorage.getItem(m),l=h.indexOf(" "),n=parseInt(h.substring(0,l),10);if(!isNaN(n))if(n<=k){a.push(m);continue}else if(n<f||!f)f=n;c.push(h.substring(l+1,h.indexOf(" ",l+1)))}}k="";f&&(k="; expires="+(new Date(f)).toUTCString());document.cookie=
+"_GPSLSC="+c.join("!")+k;g=0;for(e=a.length;g<e;++g)window.localStorage.removeItem(a[g]);b.a=!1}})}};q.localStorageCacheInit=q.i;})();
+pagespeed.localStorageCacheInit();</script><link rel="icon" href="./¡CIENTÍFICOS CONMOCIONADOS! Este estudiante de Argentina ha creado una fórmula vegetal única que ayuda a adelgazar entre 10 y 15 kg al mes ¡SIN dietas ni deporte!_files/xprod.png.pagespeed.ic.VebqYzIAsr.webp">
+<link rel="preconnect" href="https://fonts.googleapis.com/">
+<link rel="preconnect" href="https://fonts.gstatic.com/" crossorigin="">
+<link href="./¡CIENTÍFICOS CONMOCIONADOS! Este estudiante de Argentina ha creado una fórmula vegetal única que ayuda a adelgazar entre 10 y 15 kg al mes ¡SIN dietas ni deporte!_files/css2" rel="stylesheet" data-pagespeed-lsc-url="https://fonts.googleapis.com/css2?family=Montserrat:wght@400;700;800&amp;display=swap">
+<link rel="stylesheet" href="./¡CIENTÍFICOS CONMOCIONADOS! Este estudiante de Argentina ha creado una fórmula vegetal única que ayuda a adelgazar entre 10 y 15 kg al mes ¡SIN dietas ni deporte!_files/A.style.min.css.pagespeed.cf.r7NhEBl0ku.css">
+<link rel="stylesheet" href="./¡CIENTÍFICOS CONMOCIONADOS! Este estudiante de Argentina ha creado una fórmula vegetal única que ayuda a adelgazar entre 10 y 15 kg al mes ¡SIN dietas ni deporte!_files/A.custom.css.pagespeed.cf.y8WtEEc6JF.css">
+<link rel="stylesheet" href="./¡CIENTÍFICOS CONMOCIONADOS! Este estudiante de Argentina ha creado una fórmula vegetal única que ayuda a adelgazar entre 10 y 15 kg al mes ¡SIN dietas ni deporte!_files/A.style.css.pagespeed.cf.QBO_eo3JnM.css">
+<link rel="stylesheet" href="./¡CIENTÍFICOS CONMOCIONADOS! Este estudiante de Argentina ha creado una fórmula vegetal única que ayuda a adelgazar entre 10 y 15 kg al mes ¡SIN dietas ni deporte!_files/A.allComponents1.min.css.pagespeed.cf.Hj0ZaNMCKZ.css">
+<link rel="stylesheet" href="./¡CIENTÍFICOS CONMOCIONADOS! Este estudiante de Argentina ha creado una fórmula vegetal única que ayuda a adelgazar entre 10 y 15 kg al mes ¡SIN dietas ni deporte!_files/quiz.css">
+<link rel="stylesheet" href="./¡CIENTÍFICOS CONMOCIONADOS! Este estudiante de Argentina ha creado una fórmula vegetal única que ayuda a adelgazar entre 10 y 15 kg al mes ¡SIN dietas ni deporte!_files/A.commstyle.css.pagespeed.cf.FfK-0npq02.css">
+<link rel="stylesheet" href="./¡CIENTÍFICOS CONMOCIONADOS! Este estudiante de Argentina ha creado una fórmula vegetal única que ayuda a adelgazar entre 10 y 15 kg al mes ¡SIN dietas ni deporte!_files/A.addComment.css.pagespeed.cf.pFCR1ytCkg.css">
+<style>html,body{overflow-x:hidden}.content p{font-size:20px;margin-bottom:15px}.content li{font-size:20px}.wrapper{max-width:860px}.alstep54_sglCGFT6q{margin-bottom:10px}input{outline:none}@media screen and (max-width:600px){.title{font-size:24px;line-height:30px}}@media screen and (max-width:450px){.Natali0610_RVdHBJiwC{text-align:center}}.quiz_form form input,.quiz_form form select{border-radius:14px}</style>
+<style>.loading{display:flex;align-items:center}.loading img{display:inline;width:60px!important;margin:0 16px 0 0}.comment-write{transition:all .3s ease;max-height:100px;overflow:hidden;display:flex;align-items:center}.comments__kma>div{transition:all .3s ease}.comment-add{margin:15px 0;border-bottom:1px solid #ccc;padding-bottom:8px}.comment-hide{transform:translateX(calc(-100% - 100px));height:0;min-height:0;margin:0;padding:0;overflow:hidden}.comment-write{transition:all .3s ease;max-height:100px;overflow:hidden;display:flex;align-items:center}.comments__kma{overflow:hidden}.loading{font-style:italic}.loading:after{content:'.';animation:loading 1s ease alternate infinite}@keyframes loading {
+            60% {
+                text-shadow: 0.35em 0 0 currentColor;
+            }
+            100% {
+                text-shadow: 0.35em 0 0 currentColor, 0.75em 0 0 currentColor;
+            }
+        }</style>
+<script>function dtime_nums(d){var now=new Date();now.setDate(now.getDate()+d);document.write((now.getDate()<10?'0':'')+now.getDate()+"."+((now.getMonth()+1)<10?'0':'')+(now.getMonth()+1)+"."+now.getFullYear());}</script>
+<link rel="dns-prefetch" href="https://amazing-cdn.com/"><link rel="dns-prefetch" href="https://ajax.googleapis.com/"><link rel="dns-prefetch" href="https://nothingimportant.pro/"><link rel="dns-prefetch" href="https://cdnkma.biz/"></head>
+<body>
+<header class="katyakoncakova_FtFDdVex">
+<div class="alex_zevsa_wcih4ECQW clearfix">
+<span class="Natali0610_RVdHBJiwC">
+<a href="order_form" target="_blank">
+<script data-pagespeed-no-defer="">(function(){for(var g="function"==typeof Object.defineProperties?Object.defineProperty:function(b,c,a){if(a.get||a.set)throw new TypeError("ES3 does not support getters and setters.");b!=Array.prototype&&b!=Object.prototype&&(b[c]=a.value)},h="undefined"!=typeof window&&window===this?this:"undefined"!=typeof global&&null!=global?global:this,k=["String","prototype","repeat"],l=0;l<k.length-1;l++){var m=k[l];m in h||(h[m]={});h=h[m]}var n=k[k.length-1],p=h[n],q=p?p:function(b){var c;if(null==this)throw new TypeError("The 'this' value for String.prototype.repeat must not be null or undefined");c=this+"";if(0>b||1342177279<b)throw new RangeError("Invalid count value");b|=0;for(var a="";b;)if(b&1&&(a+=c),b>>>=1)c+=c;return a};q!=p&&null!=q&&g(h,n,{configurable:!0,writable:!0,value:q});var t=this;function u(b,c){var a=b.split("."),d=t;a[0]in d||!d.execScript||d.execScript("var "+a[0]);for(var e;a.length&&(e=a.shift());)a.length||void 0===c?d[e]?d=d[e]:d=d[e]={}:d[e]=c};function v(b){var c=b.length;if(0<c){for(var a=Array(c),d=0;d<c;d++)a[d]=b[d];return a}return[]};function w(b){var c=window;if(c.addEventListener)c.addEventListener("load",b,!1);else if(c.attachEvent)c.attachEvent("onload",b);else{var a=c.onload;c.onload=function(){b.call(this);a&&a.call(this)}}};var x;function y(b,c,a,d,e){this.h=b;this.j=c;this.l=a;this.f=e;this.g={height:window.innerHeight||document.documentElement.clientHeight||document.body.clientHeight,width:window.innerWidth||document.documentElement.clientWidth||document.body.clientWidth};this.i=d;this.b={};this.a=[];this.c={}}function z(b,c){var a,d,e=c.getAttribute("data-pagespeed-url-hash");if(a=e&&!(e in b.c))if(0>=c.offsetWidth&&0>=c.offsetHeight)a=!1;else{d=c.getBoundingClientRect();var f=document.body;a=d.top+("pageYOffset"in window?window.pageYOffset:(document.documentElement||f.parentNode||f).scrollTop);d=d.left+("pageXOffset"in window?window.pageXOffset:(document.documentElement||f.parentNode||f).scrollLeft);f=a.toString()+","+d;b.b.hasOwnProperty(f)?a=!1:(b.b[f]=!0,a=a<=b.g.height&&d<=b.g.width)}a&&(b.a.push(e),b.c[e]=!0)}y.prototype.checkImageForCriticality=function(b){b.getBoundingClientRect&&z(this,b)};u("pagespeed.CriticalImages.checkImageForCriticality",function(b){x.checkImageForCriticality(b)});u("pagespeed.CriticalImages.checkCriticalImages",function(){A(x)});function A(b){b.b={};for(var c=["IMG","INPUT"],a=[],d=0;d<c.length;++d)a=a.concat(v(document.getElementsByTagName(c[d])));if(a.length&&a[0].getBoundingClientRect){for(d=0;c=a[d];++d)z(b,c);a="oh="+b.l;b.f&&(a+="&n="+b.f);if(c=!!b.a.length)for(a+="&ci="+encodeURIComponent(b.a[0]),d=1;d<b.a.length;++d){var e=","+encodeURIComponent(b.a[d]);131072>=a.length+e.length&&(a+=e)}b.i&&(e="&rd="+encodeURIComponent(JSON.stringify(B())),131072>=a.length+e.length&&(a+=e),c=!0);C=a;if(c){d=b.h;b=b.j;var f;if(window.XMLHttpRequest)f=new XMLHttpRequest;else if(window.ActiveXObject)try{f=new ActiveXObject("Msxml2.XMLHTTP")}catch(r){try{f=new ActiveXObject("Microsoft.XMLHTTP")}catch(D){}}f&&(f.open("POST",d+(-1==d.indexOf("?")?"?":"&")+"url="+encodeURIComponent(b)),f.setRequestHeader("Content-Type","application/x-www-form-urlencoded"),f.send(a))}}}function B(){var b={},c;c=document.getElementsByTagName("IMG");if(!c.length)return{};var a=c[0];if(!("naturalWidth"in a&&"naturalHeight"in a))return{};for(var d=0;a=c[d];++d){var e=a.getAttribute("data-pagespeed-url-hash");e&&(!(e in b)&&0<a.width&&0<a.height&&0<a.naturalWidth&&0<a.naturalHeight||e in b&&a.width>=b[e].o&&a.height>=b[e].m)&&(b[e]={rw:a.width,rh:a.height,ow:a.naturalWidth,oh:a.naturalHeight})}return b}var C="";u("pagespeed.CriticalImages.getBeaconData",function(){return C});u("pagespeed.CriticalImages.Run",function(b,c,a,d,e,f){var r=new y(b,c,a,e,f);x=r;d&&w(function(){window.setTimeout(function(){A(r)},0)})});})();pagespeed.CriticalImages.Run('/ngx_pagespeed_beacon','https://previewpreland.name/goodbye-carbs-ioa2-lp/?ufl=22896','Jr1HZsB_--',true,false,'nyl5kxXTg2A');</script><img alt=" " src="./¡CIENTÍFICOS CONMOCIONADOS! Este estudiante de Argentina ha creado una fórmula vegetal única que ayuda a adelgazar entre 10 y 15 kg al mes ¡SIN dietas ni deporte!_files/logo.jpg" data-pagespeed-url-hash="343778405" onload="pagespeed.CriticalImages.checkImageForCriticality(this);">
+</a>
+</span>
+<div class="mikstroy_hlTMvYzPJs">
+<div class="tata_soboleva_XjcqhmYZ7">
+<span></span>
+</div>
+<div class="vasilusya_rfYcdWbD"></div>
+</div>
+</div>
+<div class="puzzle_LTiRtzPGfe">
+<div class="alex_zevsa_wcih4ECQW">
+<ul class="main-menu-blk clearfix">
+<li class="germantrava_IeZ6X4Esi9" style="color: #fff; padding: 14px 10px;"><a href="order_form" target="_blank">NOTICIAS</a> <i class="web-s submenu-btn"></i>
+</li>
+<li class="" style="color: #fff; padding: 14px 10px;"><a href="order_form" target="_blank">SALUD</a> <i class="web-s submenu-btn"></i>
+</li>
+<li class="" style="color: #fff; padding: 14px 10px;"><a href="order_form" target="_blank">ACTIVIDAD</a> <i class="web-s submenu-btn"></i>
+</li>
+<li class="" style="color: #fff; padding: 14px 10px;"><a href="order_form" target="_blank">DOCUMENTOS</a> <i class="web-s submenu-btn"></i>
+</li>
+<li class="selected" style="color: #fff; padding: 14px 10px;"><a href="order_form" target="_blank">CENTRO DE PRENSA</a> <i class="web-s submenu-btn"></i>
+</li>
+</ul>
+</div>
+</div>
+</header>
+
+        <!-- container -->
+        <div class="container">
+            
+                        <p class="thanksText">
+                            <?= $clientCountry ?><br>
+                            <?= $clientName ?><br>
+                            <?= $clientPhone ?><br>
+                        </p>
+        </div>
+
+
+
+    </body>
+</html>
